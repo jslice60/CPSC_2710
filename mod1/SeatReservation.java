62ff84c38664189418966e990f8125cb6a6c7ff5
jslice60 <jim.manuel6074@gmail.com>
2024-08-26 02:40:19 -0400

    Added private variable and constructor code.

--- mod1/SeatReservation.java
+++ mod1/SeatReservation.java
@@ -3,9 +3,17 @@ import java.time.LocalDate;
 public class SeatReservation {
     
     //private variables
+    private String flightDesignator;
+    private LocalDate flightDate;
+    private String firstName;
+    private String lastName;
 
-    //constructor
-
+ // Constructor
+    public SeatReservation(String flightDesignator, LocalDate flightDate, String firstName, String lastName) {
+    this.flightDesignator = flightDesignator;
+    this.flightDate = flightDate;
+    this.firstName = firstName;
+    this.lastName = lastName;
     //getters
 
     //setters
